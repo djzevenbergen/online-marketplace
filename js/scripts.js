e1f71d5f23fe769d76be9ee97557dd02ffdd82c3
djzevenbergen <djzevenbergen@gmail.com>
2020-03-25 14:00:49 -0700

    finish receipt

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,23 @@
 $(document).ready(function() {
+  var totalPrice;
+
   $('input:radio[name="supply"]').click(function() {
     $("#btnConfirm").removeClass("hide");
   });
 
   $("#item-form").submit(function(event) {
     $("#user-form").removeClass("hide");
+    var item = $("input:radio[name=supply]:checked").val();
+
+    if (item === "Corsair") {
+      totalPrice = 59.99 * 1.1;
+
+    } else if (item === "Asus") {
+      totalPrice = 79.99 * 1.1;
+    } else {
+      totalPrice = 99.99 * 1.1;
+    }
+    
 
     event.preventDefault();
   });
@@ -16,6 +29,7 @@ $(document).ready(function() {
     var address = $("input#address").val();
 
     $("#uName").text(name);
+    $("#price").append("<em>$" + totalPrice.toFixed(2) + "</em>");
     $("#purch").append("<em>" + item + "</em>");
     $("#uAddress").text(address); 
     $("#receipt").removeClass("hide");
